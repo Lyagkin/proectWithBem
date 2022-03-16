8674ecd43bc3043e2d42a760439216486dea9050
Viktor <lyagkin13@gmail.com>
2022-03-16 22:59:53 +0300

    Change in project. Added modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -56,4 +56,52 @@ $(document).ready(function () {
 
   toggleSlide(".catalog-item__link");
   toggleSlide(".catalog-item__back");
+
+  // Modal
+
+  $("[data-modal=consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn("slow");
+  });
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #order, #thanks").fadeOut("slow");
+  });
+  $(".button_mini").each(function (i) {
+    $(this).on("click", function () {
+      $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
+      $(".overlay, #order").fadeIn("slow");
+    });
+  });
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, укажите свое имя",
+          minlength: jQuery.validator.format("Необходимо не менее {0} символов!"),
+        },
+        phone: "Пожалуйста, укажите свой телефонный номер",
+        email: {
+          required: "Нам необходим Ваш e-mail, чтобы связаться с Вами",
+          email: "Ваш e-mail должен быть указан в формате name@domain.com",
+        },
+      },
+    });
+  }
+
+  validateForms("#consultation-form");
+  validateForms("#consultation form");
+  validateForms("#order form");
+
+  $("input[name=phone]").mask("+7 (999) 999-99-99");
 });
