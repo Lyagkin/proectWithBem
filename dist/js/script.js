9324c7958d7d56ab2c05e04e728d1f96b4cca727
Viktor <lyagkin13@gmail.com>
2023-03-17 18:01:50 +0300

    change and add slider on vanila js and change media for mobile

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,84 +1,86 @@
-// const slider = tns({
-//   container: ".carousel__inner",
-//   items: 2,
-//   slideBy: "page",
-//   autoplay: false,
-//   controls: false,
-//   nav: false,
-//   speed: 900,
-//   responsive: {
-//     320: {
-//       nav: false,
-//       edgePadding: 20,
-//       gutter: 20,
-//       items: 3,
-//     },
-//     768: {
-//       nav: false,
-//     },
-//     1440: {
-//       nav: false,
-//     },
-//     2560: {
-//       nav: false,
-//     },
-//   },
-// });
-
-// document.querySelector(".prev").addEventListener("click", function () {
-//   slider.goTo("prev");
-// });
-// document.querySelector(".next").addEventListener("click", function () {
-//   slider.goTo("next");
-// });
-
                                                                  // SLIDER
 
-const slider = document.querySelector(".carousel__slider");
-const sliderImg = document.querySelectorAll(".carousel__inner_item");
-const sliderLine = document.querySelector(".carousel__inner");
+const slider = document.querySelector(".carousel__slider"); // блок слайдера - ограничивающий контент по ширине через свойство overflow:hidden
+const sliderLine = document.querySelector(".carousel__inner"); // блок со всеми изображениями (далее слайдами)
+const sliderImgs = document.querySelectorAll(".carousel__img"); // массив изображений (далее слайдов)
+const btnNextSlide = document.querySelector(".next"); // кнопка вправо (след изображение (далее слайд))
+const btnPrevSlide = document.querySelector(".prev"); // кнопка влево (пред изображение (далее слайд))
+const dotsContainer = document.querySelector(".carousel__dots"); // блок для кнопок под изображениями (далее слайдами)
 
-const btnSliderNext = document.querySelector(".next");
-const btnSliderPrev = document.querySelector(".prev");
+const sliderWidth = slider.offsetWidth; // получение ширины блока слайдера в переменную (600px) - для определения насколько нужно смещать по оси X - горизонтальной оси слайды, чтобы они меняли друг друга
+let sliderCounter = 0; // счетчик - специальная переменная - на нее буду опираться в расчетах для свойства transform: translateX()
 
-let sliderCount = 0;
-let sliderWidth = slider.offsetWidth;
+function createDots() { // функция по динамическому созданию кнопок-точек под слайдом
 
-btnSliderNext.addEventListener("click", nextSlide);
-btnSliderPrev.addEventListener("click", prevSlide);
+  sliderImgs.forEach((slide, indexOfSlide) => { // через метод перебора массива всех слайдом - использую позицию слайдов по порядку (их индекс) - создаю в блоке для кнопок кнопки-точки опираясь на количество слайдов, а индекс помещаю в data атрибут каждой отдельной точки - в итоге количество кнопок определяется количеством слайдов и позиция точек по порядку так же соответствует позиции слайдов - их индексы идентичны
 
-function nextSlide() {
-  sliderCount++;
+    dotsContainer.insertAdjacentHTML("beforeend", // 'beforend' - использую чтобы созданные точки нумеровались по порядку начиная с 0
+    `<button class="carousel__dot" data-index=${indexOfSlide}></button>`
+    );
 
-  if (sliderCount >= sliderImg.length) {
-    sliderCount = 0;
+  });
+}
+
+createDots(); // помещаю точки в верстку
+
+function createActiveDot(i = 0) { // функция создания первой активной точки - выделяю ее цветом на фоне других, за счет добавления класса активности
+
+  document.querySelectorAll(".carousel__dot").forEach((dot) => dot.classList.remove("active")); // удаляю у всех точек класс активности
+
+  document.querySelectorAll(".carousel__dot")[i].classList.add("active"); // обращаюсь конкретно к первой точке и добавляю ей класс активности
+}
+
+createActiveDot(); // выделяю активную первую точку на странице
+
+function moveSlide(sliderCount) { // функция расчета перемещения слайдов
+
+  sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`; // смещаем блок со всеми слайдами на ширину блока слайдера - так как он вмещает только один слайд - это я установил изначально в стилях. Направление смещения зависит от переданного атрибута 'sliderCount' - изначально он будет в значении 0 - то есть на странице показан первый слайд из массива. Он указан со знаком минус - это нужно для смещения слайдов влево или вправо - логика описана в функция след слайда и пред. Далее оно умножается на ширину блока слайдера и один слайд смещается - другой его заменяет. Это значение будет соответствовать индексу слайдов и меняться в функциях повешенных на кнопки вправо/влево и кнопки/точки
+}
+
+function nextSlide() { // функция показа след слайда
+
+  if (sliderCounter >= sliderImgs.length - 1) { // логика, что я либо увеличиваю спец переменную-счетчик (изначально 0) на единицу при каждом запуске этой функции и каждый запуск этой функции активирует запуск функции расчета смещения слайдов и в ее атрибут я передаю значение переменной-счетчика - туда оно передается со знаком плюс, а далее меняется на минус (+ на - дает -) и слайды смещаются влево так как общее значение свойства translateX() со знаком минус. Либо если счетчик равен длине массива слайдов, то есть долистали до последнего слайда - я в переменную-счетчик кладу 0 и мы при след запуске этой функции смещаемся к первому слайду. Так же тут же запускается функция показа активной точки, в атрибут которой так же передаю значение счетчика - и оно указывает на индекс точки в массиве - она и выделяется на фоне других
+    sliderCounter = 0;
+  } else {
+    sliderCounter++;
   }
 
-  rollSlider();
+  moveSlide(sliderCounter);
+  createActiveDot(sliderCounter);
 }
 
-function prevSlide() {
-  sliderCount--;
-
-  if (sliderCount < 0) {
-    sliderCount = sliderImg.length - 1;
- 
+function prevSlide() { // функция показа пред слайда
+  
+  if (sliderCounter === 0) { // логика, что я либо уменьшаю спец переменную-счетчик (изначально 0) на единицу при каждом запуске этой функции и каждый запуск этой функции активирует запуск функции расчета смещения слайдов и в ее атрибут я передаю значение переменной-счетчика - туда оно передается со знаком минус, а далее меняется на плюс (- на - дает +) и слайды смещаются вправо так как общее значение свойства translateX() уменьшается. Либо если счетчик равен 0, то есть сейчас показан самый первый слайд - я в переменную-счетчик кладу значение индекса последнего слайда и мы при запуске этой функции смещаемся к последнему слайду. Так же тут же запускается функция показа активной точки, в атрибут которой так же передаю значение счетчика - и оно указывает на индекс точки в массиве - она и выделяется на фоне других
+    sliderCounter = sliderImgs.length - 1;
+  } else {
+    sliderCounter--;
   }
 
-  rollSlider();
+  moveSlide(sliderCounter);
+  createActiveDot(sliderCounter);
 }
 
-function rollSlider() {
-  sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
-}
+dotsContainer.addEventListener("click", (e) => { // обращаюсь к блоку со всеми кнопками-точками - это нужно, чтобы запустить дилегирование событий, так как точек изначально в верстке нет, то есть при клике на родителя и проверяю содержит ли элемент на который совершен клик класс точки-кнопки, если да, то получаем в переменную 'index' значение data атрибута - там лежит индекс точки - "индекс точки такой же как индекс слайда" - далее кладу это значение переменной-счетчику, и с ним запускаю функции показа активной точки и рассчет перемещения слайдов - в итоге по клику на любую кнопку-точку мы смещаемся к такому же по порядку слайду и меняем значение счетчика, чтобы это измененное значение передалось в функции повешенные на кнопки-стрелки 
 
+  const target = e.target;
 
+  if (target && target.classList.contains("carousel__dot")) {
 
+    const {index} = target.dataset;
 
+    sliderCounter = index;
 
+    createActiveDot(sliderCounter);
+    moveSlide(sliderCounter);
+  }
+});
 
+// вешаем функции на кнопки вправо/влево и по клику запускаем их 
+btnNextSlide.addEventListener("click", nextSlide);
+btnPrevSlide.addEventListener("click", prevSlide);
 
-
+////////////////////////////////////////////////////////////////////////
 
 $(document).ready(function () {
   $("ul.catalog__tabs").on("click", "li:not(.catalog__tab_active)", function () {
